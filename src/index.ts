c61aaf10f5e79e74192b0600211d5a33f758bd19
Gustavo <gu.davicosta@gmail.com>
2025-03-30 11:12:34 -0400

    feat: :sparkles: created the first route get

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import Express from 'express'
 
 const server = Express()
-
+server.get("/", (req,res)=>{
+res.send("helo word")
+})
 server.listen(8080, ()=>{
     console.log("✅ Server is running")
 })
